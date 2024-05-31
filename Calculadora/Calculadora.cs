78d0d22997fc241cf317a1c990904ed43d763a8a
Alana Silva <102611146+AlanaSilva@users.noreply.github.com>
2024-05-30 21:48:13 -0300

    Add files via upload

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        
+        public Operacoes calcular(Operacoes operacao)
+        {
+            switch(operacao.operador)
+            {
+                case '+': operacao.resultado = soma(operacao);break;
+                case '-': operacao.resultado = subtracao(operacao);break;
+                case '*': operacao.resultado = multiplicacao(operacao);break;
+                case '/': operacao.resultado = divisao(operacao);break; /* Calculo de divisão*/
+                default: operacao.resultado = 0; break;
+            }
+            return operacao;
+        }
+        public long soma(Operacoes operacao)
+        {
+            return operacao.valorA + operacao.valorB;
+        }
+        public long subtracao(Operacoes operacao)
+        {
+            return operacao.valorA - operacao.valorB;
+        }
+        public long multiplicacao(Operacoes operacao)
+        {
+            return operacao.valorA * operacao.valorB;
+        }
+
+        /* Calculo de divisão*/
+        public long divisao(Operacoes operacao)
+        {
+            return operacao.valorA / operacao.valorB;
+        }
+       
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Operacoes> filaOperacoes = new Queue<Operacoes>();
+            filaOperacoes.Enqueue(new Operacoes { valorA = 2, valorB = 3, operador = '+' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 14, valorB = 8, operador = '-' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 5, valorB = 6, operador = '*' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 2147483647L, valorB = 2, operador = '+' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' });
+
+            Calculadora calculadora = new Calculadora();
+
+            Stack<long> historico = new Stack<long>();
+
+            
+            while (filaOperacoes.Count > 0)
+            {
+                Operacoes operacao = filaOperacoes.Dequeue();
+                calculadora.calcular(operacao);
+                Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
+
+                Console.WriteLine();
+
+                OperacoesRestantes(filaOperacoes);
+                historico.Push(operacao.resultado);
+                
+            }
+            
+
+            Console.WriteLine("Histórico:");
+            foreach (long resultado in historico)
+            {
+                Console.WriteLine(resultado);
+            }
+            Console.WriteLine();
+        }
+
+        static void OperacoesRestantes(Queue<Operacoes> filaOperacoes)
+        {
+            Console.WriteLine("Operações restantes:");
+
+            foreach (Operacoes operacao in filaOperacoes)
+            {
+                Console.WriteLine("{0} {1} {2}", operacao.valorA, operacao.operador, operacao.valorB);
+            }
+            Console.WriteLine();
+        }
+    }
+}
